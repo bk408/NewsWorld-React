1a7a17b591cbf085b1388bf37edecff4b539bdc9
bk408 <bhavyakhatri1912@gmail.com>
2022-12-24 00:55:59 +0530

    newsworld create

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+export class NavBar extends Component {
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NavBar
