d04a400b40c3730fcd400d6756a144387fc1eac1
bk408 <bhavyakhatri1912@gmail.com>
2022-12-25 13:25:14 +0530

    Component structure of NewsWorld React App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return <div>
-      Hello my first class based component
-
+      <NavBar />
+      <News />
     </div>
   }
 }
---
+++ src/components/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is news item 2
+    
+      </div>
+    )
+  }
+}
+
+export default NewsItem
